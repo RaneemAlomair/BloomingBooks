f06fbf408078ef8630775e16cf3326dba62fd376
RaneemAlomair <raneemalomair11@gmail.com>
2025-08-05 10:51:35 +0300

    Initial Commit

---
+++ BloomingBooks/BloomingBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomingBooksApp.swift
+//  BloomingBooks
+//
+//  Created by Raneem Alomair on 11/02/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct BloomingBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BloomingBooksUITests/BloomingBooksUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BloomingBooksUITestsLaunchTests.swift
+//  BloomingBooksUITests
+//
+//  Created by Raneem Alomair on 11/02/1447 AH.
+//
+
+import XCTest
+
+final class BloomingBooksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
